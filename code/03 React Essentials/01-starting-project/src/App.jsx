81f8e0780d85a5b9033ee9f62231a489dfe3973c
PepeMtzCampos <pepe.mtzcampos@gmail.com>
2024-12-27 09:55:41 -0600

    Add TabButton component and examples section; implement topic selection with dynamic content

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,28 +1,38 @@
+import { useState } from 'react';
 
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
-import { CORE_CONCEPTS } from './data.js';
+import TabButton from './components/TabButton.jsx';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleTabClick(selectedButton) {
+    console.log(selectedButton + ' Button clicked!');
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
-          <h2>Core Concepts</h2>
+          {/*<h2>Core Concepts</h2> 
           <ul>
-          {/* Assigning individual props */}
+          {/* Assigning individual props /}
           <CoreConcept 
             title={CORE_CONCEPTS[0].title}
             description={CORE_CONCEPTS[0].description}
             image={CORE_CONCEPTS[0].image}
           />
-          {/* Using spread (properties names should match) */}
+          {/* Using spread (properties names should match) /}
           <CoreConcept {...CORE_CONCEPTS[1]} />
           <CoreConcept {...CORE_CONCEPTS[2]} />
           <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
-
+          */}
           <h2>Core Concepts with .map</h2>
           <ul>
             {/* Using map and spread (properties names should match) */}
@@ -30,6 +40,29 @@ function App() {
               (concept, index) => ( <CoreConcept key={index} {...concept} /> )
             )}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleTabClick('components')}>Components</TabButton>
+            <TabButton onClick={() => handleTabClick('jsx')}>JSX</TabButton>
+            <TabButton onClick={() => handleTabClick('props')}>Props</TabButton>
+            <TabButton onClick={() => handleTabClick('state')}>State</TabButton>
+          </menu>
+          
+          {!selectedTopic ? 
+            (<p>Please select a topic</p>) 
+            : 
+            (
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+              </div>
+            )
+          }
 
         </section>
       </main>
---
+++ code/03 React Essentials/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+export default function TabButton(props) {
+
+  return (
+    <li>
+      <button onClick={props.onClick}>{props.children}</button>
+    </li>
+  );
+}
