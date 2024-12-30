58ba78a9a12ab98c6d6b627f6aec685f228de737
PepeMtzCampos <pepe.mtzcampos@gmail.com>
2024-12-30 08:23:58 -0600

    Enhance TabButton component to support active state styling; update App component to reflect selected topic

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -44,10 +44,30 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={() => handleTabClick('components')}>Components</TabButton>
-            <TabButton onClick={() => handleTabClick('jsx')}>JSX</TabButton>
-            <TabButton onClick={() => handleTabClick('props')}>Props</TabButton>
-            <TabButton onClick={() => handleTabClick('state')}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'components'} 
+              onClick={() => handleTabClick('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'} 
+              onClick={() => handleTabClick('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'} 
+              onClick={() => handleTabClick('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'} 
+              onClick={() => handleTabClick('state')}
+            >
+              State
+            </TabButton>
           </menu>
           
           {!selectedTopic ? 
