0e847518de4d2f94ad38731724f56a95fef28529
Jose Martinez <jose.martinez@gm.com>
2025-01-13 09:34:03 -0600

    79. Two way binding. Update Player component to use initialName prop and manage player name state

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
 
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
     const handleEditClick = () => {
         //setIsEditing(!isEditing); This is not recommended because it might not be the correct value of isEditing
         setIsEditing((editing) => !editing);
     }
 
+    function handleInputChange(e){
+        setPlayerName(e.target.value);
+    }
+
     return(
         <li>
             <span className="player">
-                {!isEditing ? <span className="player-name">{name}</span> : <input type="text" defaultValue={name} required/>}
+                {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" value={playerName} required onChange={handleInputChange}/>}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
