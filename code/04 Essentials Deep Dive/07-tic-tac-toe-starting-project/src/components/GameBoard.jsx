e8a1b5773491fcbb8451c9be6cc97c28442468b6
PepeMtzCampos <pepe.mtzcampos@gmail.com>
2025-01-21 12:30:50 -0600

    88. Disable button for occupied squares in GameBoard component

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -29,7 +29,10 @@ export default function GameBoard( {onSelectSquare, turns} ) {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={`${rowIndex}-${colIndex}`} className="cell">
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                                  disabled={playerSymbol !== null}>
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
